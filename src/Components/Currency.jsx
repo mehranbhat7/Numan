e8982b2206d0b9d525c8251130dbf0e7f519d2a8
Mehran <mehranbhat2001@gmail.com>
2024-08-13 17:24:10 +0530

    wip

--- src/Components/Currency.jsx
+++ src/Components/Currency.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Currency = () => {
+  const [selected, setSelected] = useState("Favourites");
+
+  const handleSelect = (item) => {
+    setSelected(item);
+  };
+
   return (
     <>
       <div className="text-2xl font-extrabold font-sans ml-2 text-white mt-3 cursor-text">
         Market
+      </div>
+      <div className="flex text-white text-sm justify-around mt-3 bg-slate-600 p-2 mx-2 rounded-xl  cursor-pointer">
+        <div
+          className={`${
+            selected === "Favourites" ? "bg-green-400" : "bg-slate-300"
+          } font-medium rounded-3xl px-2 cursor-pointer`}
+          onClick={() => handleSelect("Favourites")}
+        >
+          Favourites
+        </div>
+        <div
+          className={`${
+            selected === "Gainers" ? "bg-green-400" : "bg-slate-300"
+          } font-medium rounded-3xl px-2 cursor-pointer`}
+          onClick={() => handleSelect("Gainers")}
+        >
+          Gainers
+        </div>
+        <div
+          className={`${
+            selected === "Loosers" ? "bg-green-400" : "bg-slate-300"
+          } font-medium rounded-3xl px-2 cursor-pointer`}
+          onClick={() => handleSelect("Loosers")}
+        >
+          Loosers
+        </div>
+        <div
+          className={`${
+            selected === "Volume" ? "bg-green-400" : "bg-slate-300"
+          } font-medium rounded-3xl px-2 cursor-pointer`}
+          onClick={() => handleSelect("Volume")}
+        >
+          Volume
+        </div>
       </div>
       <div className="m-2 flex items-center justify-between  bg-slate-600 rounded-xl p-3 mb-2">
         <div className="flex items-center">
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { FaUserAlt } from "react-icons/fa";
-import { BiWorld } from "react-icons/bi";
-import logo from "../assets//D0ne-logos_white.png";
+
+import { FaBars } from "react-icons/fa";
+import logo from "../assets/b2c.logo.jpg";
+
 const Navbar = () => {
   return (
     <>
-      <div className="flex justify-between  bg-gray-900 h-16  text-white items-center sticky top-0 z-40">
+      <div className="flex justify-between  bg-black h-16  text-white items-center sticky top-0 z-40">
         <div className="text-2xl ml-5 cursor-pointer text-gray-300">
-          <FaUserAlt />
+          <FaBars />
         </div>
         <div>
-          <img className="w-28" src={logo} alt="logo" />
+          <img className="w-16" src={logo} alt="logo" />
         </div>
-        <div className="text-3xl mr-5 cursor-pointer text-gray-300">
-          <BiWorld />
-        </div>
+        <div className="text-2xl mr-5 cursor-pointer text-gray-300">NAME</div>
       </div>
     </>
   );
